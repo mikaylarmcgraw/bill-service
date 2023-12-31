e88d796a2a8a242a9388ebdae3a1673c07ab1b5f
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2023-12-30 20:57:44 -0500

    Adding get single item work

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 router.Use(CORSMiddleware())
 
     router.GET("/bills", getBills)
+	router.GET("/bill/:id", getBillByID)
 	router.POST("bill", postBill)
     router.Run("localhost:8080")
 	
@@ -48,6 +49,7 @@ func postBill(c *gin.Context) {
 
 // bill represents data about a record bill.
 type bill struct {
+  ID string
   Name string 
   Amount float64
   Category string 
@@ -60,54 +62,63 @@ type bill struct {
 //dummy bill data
 var bills = []bill{
  {
+	ID: "4e8fd4e8-563e-4206-849b-88ae64efb25d",
     Name: "Mortgage",
     Amount: 3245,
     Category: "living expense",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fd4e8-563e-4206-849b-88ae66efb25d",
     Name: "Goddard Tuition",
     Amount: 2100,
     Category: "daycare",
     Frequency: "monthly",
   },
   {
+	ID: "4d8fd4e8-563e-4206-849b-88ae64efb25d",
     Name: "Groceries",
     Amount: 200,
     Category: "living expense",
     Frequency: "weekly",
   },
   {
+	ID: "4e8fd4e8-583e-4206-849b-88ae64efb25d",
     Name: "Tahoe",
     Amount: 700,
     Category: "living expense",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fd4e8-563e-4206-849b-88as64efb25d",
     Name: "TECO",
     Amount: 300,
     Category: "Utilities",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fa4e8-563e-4206-849b-88ae64efb25d",
     Name: "Water",
     Amount: 80,
     Category: "Utilities",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fd4e8-563e-4201-849b-88ae64efb25d",
     Name: "Frontier",
     Amount: 70,
     Category: "Utilities",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fd4e8-563e-4206-849b-98ae64efb25d",
     Name: "Car Insurance",
     Amount: 540,
     Category: "Insurance",
     Frequency: "monthly",
   },
   {
+	ID: "4e8fd4e8-563e-4906-849b-88ae64efb25d",
     Name: "Life Insurance",
     Amount: 90,
     Category: "insurance",
@@ -118,4 +129,20 @@ var bills = []bill{
 // getAlbums responds with the list of all albums as JSON.
 func getBills(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, bills)
+}
+
+// getBillByID locates the bill whose ID value matches the id
+// parameter sent by the client, then returns that bill as a response.
+func getBillByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range bills {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "bill not found"})
 }
